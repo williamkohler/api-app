7b4899fdfbae8195bb0251da23fef7fa769264b6
Bill Kohler <bill@thekurlandagency.com>
2018-03-30 17:47:25 -0400

    get users json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
    # ActiveModel::Serializers integrates fully into Rails controllers.
+   # https://www.simplify.ba/articles/2016/06/18/creating-rails5-api-only-application-following-jsonapi-specification/
+   # TODO: @ Media type
   def index
     users = User.all
     render json: users
